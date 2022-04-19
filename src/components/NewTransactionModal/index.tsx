942eb4f25ed55a4288699792609467d8858bcfa4
Alessandra Lima <alexiakidd.dev@gmail.com>
2022-04-19 18:56:35 -0300

    feat: adding polished and styling form

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 interface NewTransactionModalProps {
 	isOpen: boolean;
 	onRequestClose: () => void;
@@ -29,6 +31,18 @@ export const NewTransactionModal = ({
 				<h2>Cadastrar transação</h2>
 				<input placeholder="Título" />
 				<input type="number" placeholder="Valor" />
+
+				<TransactionTypeContainer>
+					<button type="button">
+						<img src={incomeImg} alt="Entrada" />
+						<span>Entrada</span>
+					</button>
+					<button type="button">
+						<img src={outcomeImg} alt="Saída" />
+						<span>Saída</span>
+					</button>
+				</TransactionTypeContainer>
+
 				<input placeholder="Categoria" />
 				<button type="submit">Cadastrar</button>
 			</Container>
